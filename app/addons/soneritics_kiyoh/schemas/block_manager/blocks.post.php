e667d84795c12549fa19287eb906eb6d7e24677e
Jordi Jolink <info@soneritics.nl>
2019-04-29 21:08:27 +0200

    Fix old school array style

--- app/addons/soneritics_kiyoh/schemas/block_manager/blocks.post.php
+++ app/addons/soneritics_kiyoh/schemas/block_manager/blocks.post.php
@@ -3,18 +3,18 @@
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  */
-$schema['soneritics_kiyoh_reviews'] = array(
-    'templates' => array(
-        'addons/soneritics_kiyoh/blocks/kiyoh_reviews.tpl' => array(),
-    ),
-    'settings' => array(
-        'review_count_per_page' => array(
+$schema['soneritics_kiyoh_reviews'] = [
+    'templates' => [
+        'addons/soneritics_kiyoh/blocks/kiyoh_reviews.tpl' => [],
+    ],
+    'settings' => [
+        'review_count_per_page' => [
             'type' => 'input',
             'default_value' => '25',
-        ),
-    ),
+        ],
+    ],
     'wrappers' => 'blocks/wrappers',
     'cache' => true
-);
+];
 
 return $schema;
