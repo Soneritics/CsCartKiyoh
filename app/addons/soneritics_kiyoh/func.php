9084daa59c25981187e2f0b83d1fe7b912f7311e
Jordi Jolink <info@soneritics.nl>
2019-05-27 22:23:19 +0200

    Add the possibility to fetch reviews from the database

--- app/addons/soneritics_kiyoh/func.php
+++ app/addons/soneritics_kiyoh/func.php
@@ -5,7 +5,7 @@ if (!defined('BOOTSTRAP')) { die('Access denied'); }
  * Get the Kiyoh API based on the CsCart settings
  * @return KiyohApi
  */
-function fn_soneritics_kiyoh_get_api()
+function fn_soneritics_kiyoh_get_api(): KiyohApi
 {
     $settings = new SoneriticsKiyohSettings;
     $api = new KiyohApi(
@@ -14,4 +14,21 @@ function fn_soneritics_kiyoh_get_api()
     );
 
     return $api;
+}
+
+/**
+ * Get reviews per page
+ * @param int $page
+ * @param int $reviewCountPerPage
+ * @return array
+ */
+function fn_soneritics_kiyoh_get_reviews(int $page, int $reviewCountPerPage = 25): array
+{
+    $start = ($page * $reviewCountPerPage) - $reviewCountPerPage;
+
+    return db_get_array(
+        "SELECT * FROM `?:soneritics_kiyoh_reviews` ORDER BY `date` DESC LIMIT ?i,?i",
+        $start,
+        $reviewCountPerPage
+    );
 }
